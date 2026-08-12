e6433f9f7ba2e4a744edee82bf7af0a12fb7b664
Milan Kněz <116789951+milanknez@users.noreply.github.com>
2026-08-12 22:30:08 +0200

    Auto-update: pages/404.php

--- pages/404.php
+++ pages/404.php
@@ -2,8 +2,8 @@
 require_once __DIR__ . '/../admin/includes/CMS.php';
 CMS::getHeader();
 ?>
-<style>* { box-sizing: border-box; } body {margin: 0;}#ijcl{font-size:5rem;color:var(--primary);}#ivgit{margin:2rem 0;font-size:1.2rem;}</style>
-<body class="bg-slate-950 text-slate-100 min-h-screen"><main><section class="section-padding"><div class="container text-center"><div class="mb-8"><h1 class="text-6xl font-serif text-[var(--primary)] mb-4" id="ijcl">404</h1><h2 class="section-title">Stránka nenalezenaq</h2></div><p class="text-xl mb-10 opacity-80" id="ivgit">
+<style>* { box-sizing: border-box; } body {margin: 0;}</style>
+<body class="bg-slate-950 text-slate-100 min-h-screen"><main><section class="section-padding"><div class="container text-center"><div class="mb-8"><h1 id="ijcl" class="text-6xl font-serif text-[var(--primary)] mb-4">404</h1><h2 class="section-title">Stránka nenalezena</h2></div><p id="ivgit" class="text-xl mb-10 opacity-80">
                     Jejda, tady nic není 👀<br/>
                     Stránka mohla být přesunuta nebo smazána.<br/>
                     Zkuste se vrátit zpět nebo přejít na úvod.
